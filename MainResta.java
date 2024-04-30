768a5e32cca0daaf84f58745dd84af63a9492cb4
Enrique Juarez <enriquejuarez.ej7@gmail.com>
2024-04-30 03:01:06 -0600

    resta con modificadores set & get

--- MainResta.java
+++ MainResta.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
+
 public class MainResta {
-public static void main(String[]args){
-Scanner opc = new Scanner(System.in);
-System.out.println("Ingresa un numero:");
-int a= opc.nextInt();
-System.out.println("Ingresa un numero:");
-int b= opc.nextInt();
-int r=a-b;
-    System.out.println("El resultado de la resta es:"+r);
-}
+    public static void main(String[] args) {
+        Scanner opc = new Scanner(System.in);
+        System.out.println("Ingresa un numero:");
+        int a = opc.nextInt();
+        System.out.println("Ingresa un numero:");
+        int b = opc.nextInt();
+        Resta r1 = new Resta();
+        r1.setA(a);
+        r1.setB(b);
+        r1.restar();
+        System.out.println(r1.restar());
+        System.out.println(r1.toString());
+    }
 }
